54eefa10c7937fd223bf0cf1e351d322a7305508
Денис Свашенко <dsvash@yandex.ru>
2022-07-07 15:43:44 +0300

    API Yatube final v1.0

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,16 +1,19 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
+from posts.models import Comment, Follow, Group, Post
 
-from posts.models import Comment, Post
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
     author = SlugRelatedField(slug_field='username', read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -19,5 +22,37 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = '__all__'
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault(),
+    )
+    following = serializers.SlugRelatedField(
+        slug_field='username', queryset=User.objects.all()
+    )
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(), fields=('user', 'following')
+            )
+        ]
+
+    def validate(self, data):
+        request_user = self.context['request'].user
+        if data['following'] == request_user:
+            raise serializers.ValidationError('На себя подписываться нельзя')
+        return data
